e29723dbd323bc08fb36dbec274850ff412fca69
cesaire <ncesairecarlos@yahoo.fr>
2026-03-22 15:21:27 +0100

    change port connection between front and back- end

--- sohblitz-frontend/services/api.ts
+++ sohblitz-frontend/services/api.ts
@@ -1,7 +1,10 @@
 //const API_URL = "http://localhost:3001" // on local
-const API_URL = "http://localhost:8080/api"  // on docker
+//const API_URL = "http://localhost:8080/api"  // in docker on local machine
+//const API_URL = process.env.NEXT_PUBLIC_API_URL;   global variable defined in .env.production and injected by docker-compose.prod.yml
+const API_URL = "http://51.75.65.254:3001" // on vps in production
 
 export async function getServices(){
+  console.log(process.env.NEXT_PUBLIC_API_URL)
   console.log("fetching services...")
     const res = await fetch(`${API_URL}/service`)
     return res.json()
